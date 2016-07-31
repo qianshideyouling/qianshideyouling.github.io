50b241e7ccf0842a12ae26980a32f46f42062534
lwg <lwg@lwg-PC>
2016-07-31 16:31:46 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# virgo
+
+A Clojure web project.
+
+## Usage
+
+have fun
+
+## License
+
+Copyright liu, weiguo, Virgo 2016 1.0
---
+++ src/core/launch.clj
@@ -0,0 +1,13 @@
+(ns core.launch
+  (:require [core.service :as service]
+            [core.helper :as helper]))
+
+(defonce start-pairs
+  [["post" "/server/server/10002"]
+   ["post" "/static/add/web/web"]])
+
+(defn -main []
+  (print "------------------ virgo start  --------------------\r\n")
+    (helper/command-line-balancer start-pairs)
+  (print "------------------virgo complete--------------------"))
+
